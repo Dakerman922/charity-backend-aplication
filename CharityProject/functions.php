6180e03ac0751e7ee9fd06f41625792a0c726806
YourKidnapper <dragonmilorn@gmail.com>
2022-10-19 01:12:57 +0300

    modified:   CharityProject/functions.php

--- CharityProject/functions.php
+++ CharityProject/functions.php
@@ -28,6 +28,7 @@ function getVolounteer($conn,$id){
 }
 
 function addVolounteer($conn,$data){
+    $id = $data['VolounteerID'];
     $fio = $data['FIO'];
     $phoneNumber = $data['TelephoneNumber'];
     $arrivalLocation = $data['ArrivalLocation'];
@@ -36,7 +37,7 @@ function addVolounteer($conn,$data){
     $capacity = $data['Capacity'];
     $booked = $data['Booked'];
     $dateAndTime = $data['DateAndTimeOfDeparture'];
-    mysqli_query($conn,"INSERT INTO heroku_00fb7a2965fdb12.volonteer_info VALUES (NULL,'$fio','$phoneNumber',' $arrivalLocation','$targetDestination','$carDescription','$capacity','$booked','$dateAndTime')");
+    mysqli_query($conn,"INSERT INTO heroku_00fb7a2965fdb12.volonteer_info VALUES ('$id','$fio','$phoneNumber',' $arrivalLocation','$targetDestination','$carDescription','$capacity','$booked','$dateAndTime')");
     
     http_response_code(201);
     
