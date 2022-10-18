16ac1135833d66a646dd572960d7fd134eb4a1be
YourKidnapper <dragonmilorn@gmail.com>
2022-10-18 15:01:58 +0300

    Update index.php

--- CharityProject/index.php
+++ CharityProject/index.php
@@ -23,5 +23,41 @@
             echo "<p>Connected</p>";
         }      
         ?>
+        <h2>Table from Mysql</h2>
+        <form method = "post">
+            <input type="hidden" name = "ShowTable">
+            <button type="submit" onclick="ShowData()">Show Table</button>
+        </form>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>FIO</th>
+                <th>Teleplhone Number</th>
+                <th>Arrival location</th>
+                <th>Target Destination</th>
+                <th>Car Description</th>
+                <th>Capacity</th>
+                <th>Booked</th>
+                <th>Date and time of departure</th>    
+            </tr>
+            <?php
+            $sql = "SELECT * FROM helpwheel.volonteer_info;";
+            $result = mysqli_query($con,$sql);
+            if($result-> num_rows > 0){
+                while($row = mysqli_fetch_array($result)){
+                    echo "<tr>";
+                    echo "<td>".$row["Volounteer ID"]."</td><td>".$row["FIO"]."</td><td>".$row["Telephone number"]."</td>";
+                    echo "<td>".$row["Arrival location"]."</td><td>".$row["Target destination"]."</td><td>".$row["Car description"]."</td>";
+                    echo "<td>".$row["Capacity"]."</td><td>".$row["Booked"]."</td><td>".$row["Date and time of departure"]."</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            }
+            else{
+                echo "0 result";
+            }
+            mysqli_close($con);
+            ?>
+        </table>
     </body>
 </html>
