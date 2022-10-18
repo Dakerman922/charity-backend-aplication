8dc94adff5ed79b7c8078058735f920e41ebc13e
YourKidnapper <dragonmilorn@gmail.com>
2022-10-18 17:48:07 +0300

    modified:   CharityProject/ConnectToDB.php

--- CharityProject/ConnectToDB.php
+++ CharityProject/ConnectToDB.php
@@ -3,7 +3,7 @@ $host="us-cdbr-east-06.cleardb.net";
 $port=3306;
 $socket="";
 $user="b2646ca55e38e5";
-$password="";
+$password="96f9b044";
 $dbname="";
 
 $conn = new mysqli($host, $user, $password, $dbname, $port, $socket)
