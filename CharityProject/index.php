cd3b8496c65d9cf26cc39db14800389d163fbfe0
YourKidnapper <dragonmilorn@gmail.com>
2022-10-18 21:41:11 +0300

    modified:   CharityProject/index.php

--- CharityProject/index.php
+++ CharityProject/index.php
@@ -3,6 +3,11 @@ require 'ConnectToDB.php';
 
 $volounteers = mysqli_query($conn,"SELECT * FROM heroku_00fb7a2965fdb12.volonteer_info");
 
-print_r($volounteers);
+$volounteers_list = [];
+
+while($volounteer = mysqli_fetch_assoc($volounteers)){
+    $volounteers_list[] = $volounteer;
+}
+print_r($volounteers_list);
 
 ?>
