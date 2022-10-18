4eaf4b53630d6369b901e9dc4cbc4152d3370e22
YourKidnapper <dragonmilorn@gmail.com>
2022-10-18 17:43:03 +0300

    modified:   CharityProject/ConnectToDB.php

--- CharityProject/ConnectToDB.php
+++ CharityProject/ConnectToDB.php
@@ -1,15 +1,16 @@
 <?php
-$host="us-cdbr-east-06.cleardb.net";
-$port=3306;
-$socket="";
-$user="b2646ca55e38e5";
-$password="96f9b044";
-$dbname="";
-
-$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db)
 	or die ('Could not connect to the database server' . mysqli_connect_error());
-    if($con->connect_error){
-        $error = $con->connect_error;
+    if($conn->connect_error){
+        $error = $conn->connect_error;
     }
 
 $con->close();
