7159289de2383ced8c5d3fcb561b1a6aaec04c00
YourKidnapper <dragonmilorn@gmail.com>
2022-10-18 17:24:33 +0300

    modified:   CharityProject/index.php

--- CharityProject/index.php
+++ CharityProject/index.php
@@ -28,23 +28,5 @@
             <input type="hidden" name = "ShowTable">
             <button type="submit" onclick="ShowData()">Show Table</button>
         </form>
-        <table>
-            <tr>
-                <th>ID</th>
-                <th>FIO</th>
-                <th>Teleplhone Number</th>
-                <th>Arrival location</th>
-                <th>Target Destination</th>
-                <th>Car Description</th>
-                <th>Capacity</th>
-                <th>Booked</th>
-                <th>Date and time of departure</th>    
-            </tr>
-            <?php
-            $sql = "SELECT * FROM heroku_00fb7a2965fdb12.volonteer_info";
-            $result = mysqli_query($con,$sql);
-            mysqli_close($con);
-            ?>
-        </table>
     </body>
 </html>
