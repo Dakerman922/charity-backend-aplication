2057dc95e18c468d9596da48081978bdc5b42252
YourKidnapper <dragonmilorn@gmail.com>
2022-10-19 10:18:47 +0300

    modified:   CharityProject/index.php

--- CharityProject/index.php
+++ CharityProject/index.php
@@ -1,8 +1,8 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: token, Content-Type');
-header('Access-Control-Allow-Methods: POST, GET, DELETE,');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Credentials: true');
 
 header("Content-type: json/application");
