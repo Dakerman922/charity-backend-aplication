ed06997d40247926be794cba7ee3c83cea0c80cc
YourKidnapper <dragonmilorn@gmail.com>
2022-10-19 23:47:16 +0300

    modified:   CharityProject/index.php

--- CharityProject/index.php
+++ CharityProject/index.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Credentials: true');
 
-header("Content-type: json/application; charset=utf-8");
+header("Content-type: json/application; charset=utf8_general_ci");
 
 
 require 'ConnectToDB.php';
